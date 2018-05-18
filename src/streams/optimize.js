9a5290c5bc45f68fd75960b6092b5afc8a50a768
Marcus Williams <marcus@dottodotdesign.co.uk>
2018-05-18 11:53:50 +0100

    update optimise setting to work with latest version of sharp

--- src/streams/optimize.js
+++ src/streams/optimize.js
@@ -1,21 +1,18 @@
 'use strict';
 
-var sharp  = require('sharp');
-var env    = require('../config/environment_vars');
-var map    = require('map-stream');
+var sharp = require('sharp');
+var env = require('../config/environment_vars');
+var map = require('map-stream');
 
-
-module.exports = function () {
-
-  return map( function (image, callback) {
-
+module.exports = function() {
+  return map(function(image, callback) {
     // pass through if there is an error
     if (image.isError()) {
       return callback(null, image);
     }
 
     // let this pass through if we are requesting the metadata as JSON
-    if (image.modifiers.action === 'json'){
+    if (image.modifiers.action === 'json') {
       image.log.log('optimize: json metadata call');
       return callback(null, image);
     }
@@ -23,44 +20,41 @@ module.exports = function () {
     image.log.time('optimize-sharp:' + image.format);
 
     var r = sharp(image.contents);
-
+    var options = {
+      overshootDeringing: true,
+      trellisQuantisation: true,
+      compressionLevel: 9
+    };
     if (env.IMAGE_PROGRESSIVE) {
-      r.progressive();
       // An advanced setting for progressive (interlace) JPEG output.
       // Calculates which spectrum of DCT coefficients uses the fewest bits.
       // Usually reduces file size at the cost of increased compression time.
-      r.optimiseScans();
+      options = Object.assign(options, {
+        progressive: true,
+        optimiseScans: true
+      });
     }
 
     // set the output quality
     if (image.modifiers.quality < 100) {
-      r.quality(image.modifiers.quality);
+      options = Object.assign(options, {
+        quality: image.modifiers.quality
+      });
     }
-
-    // An advanced setting to reduce the effects of ringing in JPEG output,
-    // in particular where black text appears on a white background (or vice versa).
-    r.overshootDeringing();
-
-    // An advanced setting to apply the use of trellis quantisation with JPEG output.
-    // Reduces file size and slightly increases relative quality at the cost of increased compression time.
-    r.trellisQuantisation();
-
-    // An advanced setting for the zlib compression level of the lossless PNG output format.
-    r.compressionLevel(9);
-
 
     // if a specific output format is specified, set it
     if (image.outputFormat) {
-      r.toFormat(image.outputFormat);
+      r.toFormat(image.outputFormat, options);
+    } else {
+      r[image.format](options);
     }
 
     // write out the optimised image to buffer and pass it on
-    r.toBuffer( function (err, buffer) {
+    r.toBuffer(function(err, buffer) {
       if (err) {
         image.log.error('optimize error', err);
         image.error = new Error(err);
-      }
-      else {
+      } else {
         image.contents = buffer;
       }
 
@@ -68,5 +62,4 @@ module.exports = function () {
       callback(null, image);
     });
   });
-
 };
